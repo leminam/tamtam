619ae2ec8b5027493ce689f96d77f290a6a85dfe
Maria Lemina <lemina.m@gmail.com>
2018-03-18 22:16:09 +0300

    add new files
    update main page
    
    add new files
    
    update webpack.config
    
    delete files

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const webpack = require('webpack')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
